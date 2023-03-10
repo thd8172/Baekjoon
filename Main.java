4810fe1bd90c26cd6fcebe07ab77848dda4d55fa
thd8172 <thd8172@naver.com>
2023-03-10 16:15:52 +0900

    bakjoon

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int s = 0; s <= i; s++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
+
---
+++ a.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class a {
+    //백준 번
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int j = sc.nextInt();
+        while (j > 0){
+            System.out.println(j);
+            j--;
+        }
+    }
+}
