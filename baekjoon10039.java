e930a0f8a1dfad32103d68ef9c0ba36998f62954
thd8172 <thd8172@naver.com>
2023-03-23 21:20:09 +0900

    baekjoon

---
+++ baekjoon10039.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class baekjoon10039 {
+    public static void main(String[] args) {
+        //알고리즘 10039번
+        Scanner sc = new Scanner(System.in);
+
+        int sum = 0;
+
+        for (int i = 0; i < 5; i++) {
+            int y = sc.nextInt();
+            sum += y > 40 ? y : 40;
+        }
+        System.out.println(sum / 5);
+    }
+}
