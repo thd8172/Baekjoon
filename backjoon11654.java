e6afc56a56a00f959c3b863be76f6f175760dae2
thd8172 <113906692+thd8172@users.noreply.github.com>
2024-04-23 23:35:04 +0900

    Create backjoon11654.java

---
+++ backjoon11654.java
@@ -0,0 +1,13 @@
+package 백준;
+import java.util.Scanner;
+public class backjoon11654 {
+
+		public static void main(String[] args) {
+	 
+			Scanner in = new Scanner(System.in);
+	 
+			int ch = in.next().charAt(0);
+	        
+			System.out.print(ch);
+		}
+	}
