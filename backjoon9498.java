be893e352b47629fb058c1ba47449f71bec682b7
thd8172 <113906692+thd8172@users.noreply.github.com>
2024-04-24 23:40:03 +0900

    Create backjoon9498.java

---
+++ backjoon9498.java
@@ -0,0 +1,28 @@
+package 백준;
+import java.util.Scanner;
+public class backjoon9498 {
+
+	import java.util.Scanner;
+
+	public class Main {
+	        public static void main(String[] args) {
+	            Scanner in = new Scanner(System.in);
+	            int num = (new Scanner(System.in)).nextInt();
+	            in.close();
+	            if
+	            (num>=90) {
+	                System.out.println("A");
+	            }
+	          else if (num>= 80)
+	                    System.out.println("B");
+	           else if (num >= 70)
+	                    System.out.println("C");
+	                 else if (num >= 60)
+	                    System.out.println("D");
+	                else System.out.println("F");
+
+	            }
+	        }
+
+		}
+	}
